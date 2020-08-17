2fd273afaca280c614266a24e9ecabbbbe344801
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2020-08-17 21:34:32 +0300

    student homework example

---
+++ src/com/javaguru/student_name_lastname/lesson_1/level_4/Task_10.java
@@ -0,0 +1,9 @@
+package com.javaguru.student_name_lastname.lesson_1.level_4;
+
+class Task_10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Ruslan");
+    }
+}
