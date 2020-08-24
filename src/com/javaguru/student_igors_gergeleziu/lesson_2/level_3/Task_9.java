03072e8b3571c3254a85520e4eba10ba3598e913
MrJohnyRony <disqueeze@mail.ru>
2020-08-25 00:28:43 +0300

    Used cicle for

---
+++ src/com/javaguru/student_igors_gergeleziu/lesson_2/level_3/Task_9.java
@@ -0,0 +1,16 @@
+package com.javaguru.student_igors_gergeleziu.lesson_2.level_3;
+import java.util.Scanner;
+
+public class Task_9 {
+
+    public static void main(String[] args) {
+        System.out.println("Test Data: ");
+        System.out.println("Input a number: ");
+        Scanner sc= new Scanner(System.in);
+        int inputNumber=sc.nextInt();
+        System.out.println("Expected Output : ");
+        for(int i=1; i<=10; i++){
+            System.out.println(inputNumber+" x "+i+" = "+inputNumber*i);
+        }
+    }
+}
