034c770bb5b6847fc67938fbee7d97b39b2a9330
IvanShulga <glik@bk.ru>
2020-08-24 22:11:10 +0300

    homework

---
+++ src/com/javaguru/student_ivan_shulga/lesson_1/level_4/Task_10.java
@@ -0,0 +1,11 @@
+package com.javaguru.student_ivan_shulga.lesson_1.level_4;
+
+class Task_10 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Ivan Shulga");
+
+    }
+}
