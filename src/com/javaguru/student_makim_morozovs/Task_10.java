10d718ed81b38a019184c0554d162e64db5ecb3f
Maks <maksims.morozovs.11@gmail.com>
2020-08-24 22:30:51 +0300

    Homework

---
+++ src/com/javaguru/student_makim_morozovs/Task_10.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_makim_morozovs;
+
+public class Task_10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Maksim");
+
+    }
+}
---
+++ src/com/javaguru/student_makim_morozovs/Task_11.java
@@ -0,0 +1,16 @@
+package com.javaguru.student_makim_morozovs;
+
+class Task_11 {
+    public static void main(String[] args) {
+        int number1 = 74;
+        int number2 = 36;
+        int sumOfNumbers = number1 + number2;
+        System.out.println(sumOfNumbers);
+        System.out.println();
+
+        int number3 = 50;
+        int number4 = 3;
+        int divideNumbers = number3 / number4;
+        System.out.println(divideNumbers);
+    }
+}
