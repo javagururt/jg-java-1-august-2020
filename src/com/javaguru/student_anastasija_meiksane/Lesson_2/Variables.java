00e31f99d566d213842a98a1b3128a70d4ed2c4d
anameiksana <69810552+anameiksana@users.noreply.github.com>
2020-08-24 21:20:28 +0300

    ==

---
+++ src/com/javaguru/student_anastasija_meiksane/Lesson_2/Variables.java
@@ -0,0 +1,19 @@
+package com.javaguru.student_anastasija_meiksane.Lesson_2;
+
+class human {
+
+    public static void main(String[] args) {
+
+        int age = 28;
+        System.out.println(age);
+        boolean hungry = true;
+        System.out.println(hungry);
+        System.out.println(age + "" + hungry);
+
+        String name = "Anna";
+        System.out.println(hungry + name + age);
+
+
+    }
+}
+
