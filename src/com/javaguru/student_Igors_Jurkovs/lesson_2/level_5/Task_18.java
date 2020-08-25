6dc82b81fa910c16ebc1c4b6299ac248aca8f09a
Игорь <revqqq@gmail.com>
2020-08-25 20:41:05 +0300

    homework lesson 2 Tasks 15-21

---
+++ src/com/javaguru/student_Igors_Jurkovs/lesson_2/level_5/Task_18.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_Igors_Jurkovs.lesson_2.level_5;
+
+class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+
+        System.out.println("You are fixed now!");
+    }
+}
