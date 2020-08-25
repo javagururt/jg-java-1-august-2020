757fa5f42281e2ae8c46defb382e3762bad1988c
Игорь <revqqq@gmail.com>
2020-08-25 12:11:24 +0300

    homework lesson 2 Tasks 4-8, moved to levels

--- src/com/javaguru/student_Igors_Jurkovs/lesson_2/level_2/Task_7.java
+++ src/com/javaguru/student_Igors_Jurkovs/lesson_2/level_2/Task_7.java
@@ -1,4 +1,4 @@
-package com.javaguru.student_Igors_Jurkovs.lesson_2;
+package com.javaguru.student_Igors_Jurkovs.lesson_2.level_2;
 
 class LoginService {
     public static void main(String[] args) {
--- src/com/javaguru/student_Igors_Jurkovs/lesson_2/level_2/Task_8.java
+++ src/com/javaguru/student_Igors_Jurkovs/lesson_2/level_2/Task_8.java
@@ -1,4 +1,4 @@
-package com.javaguru.student_Igors_Jurkovs.lesson_2;
+package com.javaguru.student_Igors_Jurkovs.lesson_2.level_2;
 
 import java.util.Scanner;
 
