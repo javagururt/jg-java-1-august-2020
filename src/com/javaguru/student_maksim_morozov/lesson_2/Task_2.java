f628d52fcd108475c3fa2c74af0fb753625d6917
Maks <maksims.morozovs.11@gmail.com>
2020-08-26 15:59:23 +0300

    Homework

--- src/com/javaguru/student_maksim_morozov/lesson_2/Task_2.java
+++ src/com/javaguru/student_maksim_morozov/lesson_2/Task_2.java
@@ -1,4 +1,15 @@
 package com.javaguru.student_maksim_morozov.lesson_2;
 
-public class Task_2 {
-}
+import java.util.Scanner;
+        class Task_2{
+            public static void main (String[]args){
+                Scanner sc = new Scanner(System.in);
+                double firstDoubleNumber = sc.nextDouble();
+                double secondDoubleNumber = sc.nextDouble();
+                System.out.println(firstDoubleNumber + secondDoubleNumber);
+                System.out.println(firstDoubleNumber - secondDoubleNumber);
+                System.out.println(firstDoubleNumber * secondDoubleNumber);
+                System.out.println(firstDoubleNumber / secondDoubleNumber);
+            }
+            
+        }
