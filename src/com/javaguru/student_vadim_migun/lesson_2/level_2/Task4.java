f820b89a20bcde0ea02b57db509759e87792b418
Vadim <vadimka193308@gmail.com>
2020-08-27 23:01:41 +0300

    Homework

--- src/com/javaguru/student_vadim_migun/lesson_2/level_2/Task4.java
+++ src/com/javaguru/student_vadim_migun/lesson_2/level_2/Task4.java
@@ -1,7 +1,7 @@
 package com.javaguru.student_vadim_migun.lesson_2.level_2;
 
 class Phone {
-    public static void main (System [] args) {
+    public static void main (String [] args) {
 
     }
 
