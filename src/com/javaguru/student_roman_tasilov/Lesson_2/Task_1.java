245c786aa9d831ebbfd06d07b03d7141138f1cbb
Roman0954 <tashilov@gmail.com>
2020-08-28 14:09:15 +0300

    task

---
+++ src/com/javaguru/student_roman_tasilov/Lesson_2/Task_1.java
@@ -0,0 +1,21 @@
+package com.javaguru.student_roman_tasilov.Lesson_2;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        int a = 74;
+        int b = 36;
+        int c = a + b;
+        System.out.println(c);
+
+        int d = 5;
+        int e = 10;
+        int f = e / d;
+        System.out.println("Result:" + f);
+
+        int g = f * c;
+        System.out.println("g =" + g);
+
+        int x = g - a;
+        System.out.println(x);
+    }
+}
