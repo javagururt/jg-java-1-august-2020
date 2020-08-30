eee4432ff106da1e210eeb8cc0517798bacd0724
Nikolajs <n.sersnovs@gmail.com>
2020-08-30 22:57:52 +0300

    Please check. all done.

---
+++ src/com/javaguru/student_nikolajs_sersnovs/leason_2/level_6/Task_23.java
@@ -0,0 +1,9 @@
+package com.javaguru.student_nikolajs_sersnovs.leason_2.level_6;
+
+class AttentionPlease {
+
+    public static void main(String[] args) {
+    System.out.println("Code outside the method will not compile!");
+
+    }
+}
---
+++ src/com/javaguru/student_nikolajs_sersnovs/leason_2/level_x/Super_task_3.java
@@ -0,0 +1,16 @@
+package com.javaguru.student_nikolajs_sersnovs.leason_2.level_x;
+
+class CorrectDataTypes {
+
+    public static void main(String[] args) {
+
+        double numberOne = 10;
+        double numberTwo = 20.0;
+        double sum = numberOne + numberTwo;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+        System.out.println("Sum = " + sum);
+    }
+
+}
