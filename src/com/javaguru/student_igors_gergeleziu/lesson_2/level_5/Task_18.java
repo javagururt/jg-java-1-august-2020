1b78c78c34ef1667b580c8b5afef89336881965e
MrJohnyRony <disqueeze@mail.ru>
2020-08-31 21:00:24 +0300

    System and ;

---
+++ src/com/javaguru/student_igors_gergeleziu/lesson_2/level_5/Task_18.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_igors_gergeleziu.lesson_2.level_5;
+
+    class SizeMatters {
+        public static void main(String[] args) {
+            System.out.println("Sorry, I have an errors.");
+            System.out.println("Please fix me!");
+        }
+    }
+
+
