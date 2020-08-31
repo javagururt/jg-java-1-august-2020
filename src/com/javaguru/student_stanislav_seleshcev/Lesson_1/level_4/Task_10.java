65037665f8b5097a76baaf146c9cd72ad26f0925
facedown7 <stan.selv@gmail.com>
2020-08-31 03:15:00 +0300

    Stanislav Seleshcev first hometask

---
+++ src/com/javaguru/student_stanislav_seleshcev/Lesson_1/level_4/Task_10.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_stanislav_seleshcev.Lesson_1.level_4;
+
+public class Task_10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Stanislav Seleschev");
+    }
+}
+
