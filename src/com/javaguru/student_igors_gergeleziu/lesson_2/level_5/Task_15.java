091ab76cf491c25ea892f9acca36a274cd2cfe8e
MrJohnyRony <disqueeze@mail.ru>
2020-08-31 20:57:03 +0300

    Done

---
+++ src/com/javaguru/student_igors_gergeleziu/lesson_2/level_5/Task_15.java
@@ -0,0 +1,18 @@
+package com.javaguru.student_igors_gergeleziu.lesson_2.level_5;
+
+public class Task_15 {
+    public static void main(String[] args) {
+        System.out.println("Separately");
+        System.out.println("Sep");
+        System.out.println("ara");
+        System.out.println("tely");
+        System.out.print("Sep ");
+        System.out.print(" ara ");
+        System.out.println(" tely");
+        System.out.println("Together");
+        System.out.print("Tog ");
+        System.out.print(" eth ");
+        System.out.print(" er ");
+        System.out.print("Together");
+    }
+}
