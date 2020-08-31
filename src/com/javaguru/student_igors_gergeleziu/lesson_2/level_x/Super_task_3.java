4d388f7c0425fbb51a981d03e5b4e9c295c127eb
MrJohnyRony <disqueeze@mail.ru>
2020-08-31 21:49:47 +0300

    Done

---
+++ src/com/javaguru/student_igors_gergeleziu/lesson_2/level_x/Super_task_3.java
@@ -0,0 +1,22 @@
+package com.javaguru.student_igors_gergeleziu.lesson_2.level_x;
+
+class CorrectDataTypes {
+
+        public static void main(String[] args) {
+
+            int numberOne = 10;
+            double numberTwo = 20.0;
+
+            System.out.println("Number 1 = " + numberOne);
+            System.out.println("Number 2 = " + numberTwo);
+
+            float sum = numberOne + (float)numberTwo;
+            //double sum = numberOne + numberTwo;
+
+            System.out.println("Sum = " + sum);
+        }
+
+    }
+
+
+
