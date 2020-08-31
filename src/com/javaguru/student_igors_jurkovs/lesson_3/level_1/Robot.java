2ccb541c916c851cf7e584450e987d1c2e201401
Игорь <revqqq@gmail.com>
2020-09-01 00:08:37 +0300

    Robot

---
+++ src/com/javaguru/student_igors_jurkovs/lesson_3/level_1/Robot.java
@@ -0,0 +1,26 @@
+package com.javaguru.student_igors_jurkovs.lesson_3.level_1;
+
+class Robot {
+    public Robot() {
+
+    }
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
+
+class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot robot = new Robot();
+        robot.sayHello();
+        robot.sayYourName();
+
+        Robot robot2 = new Robot();
+        robot2.sayHello();
+        robot2.sayYourName();
+    }
+}
