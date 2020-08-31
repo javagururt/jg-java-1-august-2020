68d7ca58267d7792dd19bf043c088f5725d20ee1
facedown7 <stan.selv@gmail.com>
2020-08-31 03:26:20 +0300

    Stanislav Seleshcev first hometask

--- src/com/javaguru/student_stanislav_seleshcev/Task_10.java
+++ src/com/javaguru/student_stanislav_seleshcev/Task_10.java
@@ -1,4 +1,4 @@
-package com.javaguru.student_stanislav_seleshcev.Lesson_1.level_4;
+package com.javaguru.student_stanislav_seleshcev;
 
 public class Task_10 {
     public static void main(String[] args) {
