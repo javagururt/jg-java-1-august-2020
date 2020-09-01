97603aa9004267d5a78a3ef6a4f0bd0785433793
IvanShulga <glik@bk.ru>
2020-09-01 21:09:31 +0300

    homework

--- src/com/javaguru/student_ivan_shulga/lesson_3/level_2/Task_8.java
+++ src/com/javaguru/student_ivan_shulga/lesson_3/level_2/Task_8.java
@@ -1,11 +1,12 @@
 package com.javaguru.student_ivan_shulga.lesson_3.level_2;
 
-class Task_7 {
+class Task_8 {
 
     public static void main(String[] args) {
 
         String myName = "Ivan";
-        System.out.println(myName);
+        String greeting = "Hi " + myName + "!";
+        System.out.println(greeting);
 
     }
 
