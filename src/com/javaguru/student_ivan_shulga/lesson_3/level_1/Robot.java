dc12006ab9114f28d48fb7a1b06ab59d6e6276f2
IvanShulga <glik@bk.ru>
2020-09-01 20:07:50 +0300

    homework

---
+++ src/com/javaguru/student_ivan_shulga/lesson_3/level_1/Robot.java
@@ -0,0 +1,20 @@
+package com.javaguru.student_ivan_shulga.lesson_3.level_1;
+
+// пока что для себя буду порой писать много комментариев, чтобы самому разобраться что к чему, т.к.
+// после "Hello world!" для меня начинается тёмный лес в Java
+
+class Robot {    // создаём класс  Robot (Task_1)
+
+    public void sayHello() {
+
+        System.out.println("Hello!");  // тут пишем команду распечатки на консоли строки "Hello!" (Task_1)
+    }
+
+    public Robot() {    // в классе робот (Robot) создаём конструктор без параметров (Task_2)
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO!");  // тут пишем команду распечатки на консоли строки "My name is ROBO" (Task_5)
+    }
+
+}
---
+++ src/com/javaguru/student_ivan_shulga/lesson_3/level_2/Task_7.java
@@ -0,0 +1,4 @@
+package com.javaguru.student_ivan_shulga.lesson_3.level_2;
+
+public class Task_7 {
+}
