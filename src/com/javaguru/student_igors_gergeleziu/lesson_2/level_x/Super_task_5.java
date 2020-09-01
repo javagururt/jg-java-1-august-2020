3bc51945d738532c1e5e5c99b303ddf699751b45
MrJohnyRony <disqueeze@mail.ru>
2020-09-01 20:46:15 +0300

    Done

---
+++ src/com/javaguru/student_igors_gergeleziu/lesson_2/level_x/Super_task_5.java
@@ -0,0 +1,12 @@
+package com.javaguru.student_igors_gergeleziu.lesson_2.level_x;
+
+public class Super_task_5 {
+
+    public static void main(String[] args) {
+        /*
+        Multi-line comment
+         */
+
+        //One-line comment
+    }
+}
