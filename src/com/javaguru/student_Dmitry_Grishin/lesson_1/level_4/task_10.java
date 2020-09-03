1d5d2ac5946ffee137fefc4bda94d9f06962d0b6
DmitryGrishin1805 <dg9261115311@gmail.com>
2020-09-03 16:39:27 +0300

    studente homework example1

---
+++ src/com/javaguru/student_Dmitry_Grishin/lesson_1/level_4/task_10.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_Dmitry_Grishin.lesson_1.level_4;
+
+public class task_10 {
+
+    public static void main(String[] args){
+       System.out.println("Hello World");
+    }
+}
