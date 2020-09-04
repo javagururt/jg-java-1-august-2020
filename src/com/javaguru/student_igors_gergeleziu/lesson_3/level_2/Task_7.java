68ab939d205df3407a8585b8d419fbf68c28e144
MrJohnyRony <disqueeze@mail.ru>
2020-09-04 19:23:35 +0300

    Done

---
+++ src/com/javaguru/student_igors_gergeleziu/lesson_3/level_2/Task_7.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_igors_gergeleziu.lesson_3.level_2;
+
+public class Task_7 {
+    public static void main(String[] args) {
+        String myName="Igors";
+        System.out.println(myName);
+    }
+}
