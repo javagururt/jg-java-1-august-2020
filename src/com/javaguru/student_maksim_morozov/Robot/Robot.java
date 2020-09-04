2ebbf4c66f5f534875f4a749e038283cad0ec5dc
Maks <maksims.morozovs.11@gmail.com>
2020-09-04 13:52:15 +0300

    Homework

---
+++ src/com/javaguru/student_maksim_morozov/Robot/Robot.java
@@ -0,0 +1,9 @@
+package com.javaguru.student_maksim_morozov.Robot;
+
+class Robot { public Robot() {
+}
+    public void sayHello() {
+        System.out.println("Hello!"); }
+    public void sayYourName() {
+        System.out.println("My name is R2D2"); }
+}
---
+++ src/com/javaguru/student_maksim_morozov/Robot/RobotDemo.java
@@ -0,0 +1,9 @@
+package com.javaguru.student_maksim_morozov.Robot;
+
+class RobotDemo {
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+        robot.sayHello();
+        robot.sayYourName();
+    }
+}
