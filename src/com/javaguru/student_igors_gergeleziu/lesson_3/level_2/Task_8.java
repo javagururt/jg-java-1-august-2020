0f014c58f73afc849b9a1862ea9b9fd7b04efb4e
MrJohnyRony <disqueeze@mail.ru>
2020-09-04 19:27:36 +0300

    Done

---
+++ src/com/javaguru/student_igors_gergeleziu/lesson_3/level_2/Task_8.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_igors_gergeleziu.lesson_3.level_2;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        String userName= "Igors";
+        String greeting="Hi "+ userName+ "!";
+        System.out.println(greeting);
+    }
+
+}
