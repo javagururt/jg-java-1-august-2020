2555b97de60a9495717806b1f368d5401be6ff8f
IvanShulga <glik@bk.ru>
2020-09-07 00:07:18 +0300

    homework

--- src/com/javaguru/student_ivan_shulga/lesson_3/level_6/Task_26.java
+++ src/com/javaguru/student_ivan_shulga/lesson_3/level_6/Task_26.java
@@ -1,19 +1,24 @@
 package com.javaguru.student_ivan_shulga.lesson_3.level_6;
-/*
+
 class Product {
 
-    private String name;
-    private double regularPrice;
-    private double discount;
+    String name;            // наименование
+    double regularPrice;    // стандартная цена
+    double discount;        // скидка в процентах
+    // ^^^ объявляем переменные
 
     public Product(String name) { this.name = name; }
-    public void actualPrice(double actualPrice) { actualPrice = regularPrice * discount;}
+    // ^^^ Присвоение наименования продукта переменной name
+
+    public double actualPrice() { return regularPrice - (regularPrice / 100 * discount); }
+    // ^^^ Метод расчёта цены со скидкой
 
     public void printInformation() {
         System.out.println("Product: " + name);
         System.out.println("Regular price: " + regularPrice);
-        System.out.println("Discount: " + discount);
-        System.out.println("Price with discount: " + actualPrice);
+        System.out.println("Discount: " + discount + "%");
+        System.out.println("Price with discount: " + actualPrice());
+        // ^^^ Вывод информации
     }
 
 }
@@ -23,11 +28,18 @@ class ProductDemo {
     public static void main(String[] args) {
 
         Product product = new Product("Milk");
-        product.regularPrice(100);
-        product.discount(0.10);
-        //product.actualPrice();
+        // ^^^ Создание нового товара с помощью конструктора и присвоение ему имени
+
+        product.regularPrice = 100;
+        product.discount = 25;
+        // ^^^ Задаём стоимость и скидку через оператор
+
+        product.actualPrice();
+        // ^^^ Вызов метода расчёта цены со скидкой для новосозданного товара
+
+        product.printInformation();
+        // ^^^ Вызов метода вывода информации на экран для новара
 
     }
 
 }
-*/
