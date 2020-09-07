075dd20eab1e463f5790761d198ab5e072decddf
Игорь <revqqq@gmail.com>
2020-09-07 15:29:55 +0300

    ошибку выдает

--- src/com/javaguru/student_vadim_migun/lesson_2/level_2/Book.java
+++ src/com/javaguru/student_vadim_migun/lesson_2/level_2/Book.java
@@ -1,11 +1,11 @@
 package com.javaguru.student_vadim_migun.lesson_2.level_2;
 
-class Book {
+/*class Book {
 
       public static void main(String [] args){
       }
 
-}
+}*/ //java: duplicate class: com.javaguru.student_vadim_migun.lesson_2.level_2.Book
 
 
 /*
