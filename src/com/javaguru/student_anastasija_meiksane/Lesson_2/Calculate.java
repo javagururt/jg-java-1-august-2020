fe9c6a77c3fc050d2c8bc744ffe031e2f89dd7f2
anameiksana <69810552+anameiksana@users.noreply.github.com>
2020-09-07 14:57:20 +0300

    ==

---
+++ src/com/javaguru/student_anastasija_meiksane/Lesson_2/Calculate.java
@@ -0,0 +1,12 @@
+package com.javaguru.student_anastasija_meiksane.Lesson_2;
+
+class Calculate {
+
+    public static void main(String[] args) {
+
+        int a = 5 ;
+        int b = 2;
+        double calculate = (a / (double) b) ;
+        System.out.println(calculate);
+    }
+}
