a344fa68ee7d89fd83352d59988cd5ded58e47f6
MrJohnyRony <disqueeze@mail.ru>
2020-09-08 20:18:57 +0300

    Done

---
+++ src/com/javaguru/student_igors_gergeleziu/lesson_3/level_5/DogDemo.java
@@ -0,0 +1,11 @@
+package com.javaguru.student_igors_gergeleziu.lesson_3.level_5;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog buddy= new Dog("Buddy", 5, "black");
+        buddy.voice();
+        buddy.happyBirthday();
+        buddy.changeColor("white");
+        buddy.voice();
+    }
+}
