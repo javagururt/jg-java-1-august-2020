a8a8ce8cf69a9e2086cbd9920340448ec79ecf38
VladimirsFilipovs <70902436+VladimirsFilipovs@users.noreply.github.com>
2020-09-08 17:13:34 +0300

    #Lesson1 homework

---
+++ src/com/javaguru/student_vladimirs_filipovs/lesson_1/level_4/Task_11.java
@@ -0,0 +1,13 @@
+package com.javaguru.student_vladimirs_filipovs.lesson_1.level_4;
+
+class Task_11 {
+    public static void main (String[] args){
+        int firstNumber1 = 74;
+        int secondNumber1 = 36;
+        System.out.println(firstNumber1 + secondNumber1);
+
+        int firstNumber2 = 50;
+        int secondNumber2 = 3;
+        System.out.println(firstNumber2 / secondNumber2);
+    }
+}
