01c092939038e62634a8ae69a76a464d7c535142
Игорь <revqqq@gmail.com>
2020-09-09 16:49:39 +0300

    homework

---
+++ src/com/javaguru/student_igors_jurkovs/lesson_5/level_1_intern/ArrayDefinition5.java
@@ -0,0 +1,9 @@
+package com.javaguru.student_igors_jurkovs.lesson_5.level_1_intern;
+
+class ArrayDefinition5 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, };
+    }
+
+}
