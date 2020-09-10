3deca79322902146d3d43dc7378b94d88c318cd8
Игорь <revqqq@gmail.com>
2020-09-10 22:34:33 +0300

    homework

--- src/com/javaguru/student_igors_jurkovs/lesson_5/level_2_intern/Array3.java
+++ src/com/javaguru/student_igors_jurkovs/lesson_5/level_2_intern/Array3.java
@@ -2,24 +2,19 @@ package com.javaguru.student_igors_jurkovs.lesson_5.level_2_intern;
 /*
 Написать программу, в которой:
  - создать массив целых чисел длиной 3,
- - заполнить каждую ячейку массива любым числом,
- - вывести значение каждой ячейки на консоль.
- */
-/*
-Написать программу, в которой:
- - создать массив целых чисел длиной 3,
  - заполнить каждую ячейку массива случайным числом,
- - вывести значение каждой ячейки на консоль.
+ - посчитать сумму всех ячеек и вывести ее на консоль.
 
  */
-class Array {
+class Array3 {
     public static void main(String[] args) {
 
         int numbers[] = new int[3];
+        int sumOfNumbers = 0;
         for(int i = 0; i < 3; i++) {
             numbers[i] =(int) (Math.random() * 10);
-            System.out.println(numbers[i]);
+            sumOfNumbers += numbers[i];
         }
-
+        System.out.println(sumOfNumbers);
     }
 }
---
+++ src/com/javaguru/student_igors_jurkovs/lesson_5/level_4_junior/MaxFromArray.java
@@ -0,0 +1,23 @@
+package com.javaguru.student_igors_jurkovs.lesson_5.level_4_junior;
+/*
+Напишите программу, в которой:
+- создайте массив произвольной длины
+- заполните массив случайными числами
+- распечатайте на консоль все элементы массива
+- найдите наибольшее число в массиве и выведети его на консоль.
+ */
+
+import java.util.Arrays;
+
+class MaxFromArray {
+    public static void main(String[] args) {
+        int index = (int) (Math.random() * 100);
+        int numbers[] = new int[index];
+        for (int i = 0; i < index; i++) {
+            numbers[i] = (int) (Math.random() * 10000);
+            System.out.println(numbers[i]);
+        }
+        Arrays.sort(numbers);
+        System.out.println("Max number in array is = " + numbers[index - 1]);
+    }
+}
