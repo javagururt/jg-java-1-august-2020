8d06d4806d071486f7193ed297b2d6d0609efcdb
Maks <maksims.morozovs.11@gmail.com>
2020-09-11 13:41:35 +0300

    Homework

--- src/com/javaguru/student_maksim_morozov/lesson_3/Level_5/Dog.java
+++ src/com/javaguru/student_maksim_morozov/lesson_3/Level_5/Dog.java
@@ -4,4 +4,17 @@ class Dog {
     private String name;
     private int age;
     private String color;
+
+    public Dog (String dogName, int age, String color) {
+    this.name = dogName;
+    this.age = age;
+    this.color = color; }
+
+    public void voice(){
+        System.out.println("Dog Name:" + name + ", Dog Age:" + age + ", Dog Color:" + color);
+        System.out.println("Dog Name:" + name + ", Dog Age:" + age + ", Dog Color:" + color);
+        System.out.println("Dog Name:" + name + ", Dog Age:" + age + ", Dog Color:" + color);
+    }
+    public void happyBirtDay(){this.age += 1; }
+    public void changeColor (String newColor) {this.color = newColor;}
 }
---
+++ src/com/javaguru/student_maksim_morozov/lesson_3/Level_5/DogDemo.java
@@ -0,0 +1,11 @@
+package com.javaguru.student_maksim_morozov.lesson_3.Level_5;
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+    Dog dog = new Dog("Korbi",3,"White");
+    dog.voice();
+    dog.happyBirtDay();
+    dog.changeColor("Beige");
+    dog.voice();}
+    }
