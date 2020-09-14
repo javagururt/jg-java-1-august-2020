49d1244aeae0f3faab1db2404d15a7a30c20e154
Maks <maksims.morozovs.11@gmail.com>
2020-09-14 12:17:55 +0300

    Homework

---
+++ src/com/javaguru/student_maksim_morozov/level_1_intern/Task_2.java
@@ -0,0 +1,19 @@
+package com.javaguru.student_maksim_morozov.level_1_intern;
+
+import java.util.Scanner;
+
+class Task_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter integer value: ");
+        int number = scan.nextInt();
+        System.out.println("Number = " + number);
+        if (number > 0) {
+            System.out.println("The number is positive!");
+        } else if (number < 0) {
+            System.out.println("The number is negative!");
+        } else {
+            System.out.println("The number = 0!");
+        }
+    }
+}
---
+++ src/com/javaguru/student_maksim_morozov/level_1_intern/Task_3.java
@@ -0,0 +1,27 @@
+package com.javaguru.student_maksim_morozov.level_1_intern;
+
+import java.util.Scanner;
+
+class Task_3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Write the day of the week (in numbers): ");
+        int number = scan.nextInt();
+        switch (number){
+            case 1: System.out.println("Monday!");
+                break;
+            case 2: System.out.println("Tuesday!");
+                break;
+            case 3: System.out.println("Wednesday!");
+                break;
+            case  4: System.out.println("Thursday!");
+                break;
+            case 5: System.out.println("Friday!");
+                break;
+            case 6: System.out.println("Saturday!");
+                break;
+            case 7: System.out.println("Sunday!");
+                break;
+        }
+    }
+}
