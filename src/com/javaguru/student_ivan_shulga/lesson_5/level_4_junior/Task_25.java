96e1959faf813f96851b3162e9fb4d8b150f3b65
IvanShulga <glik@bk.ru>
2020-09-18 18:53:25 +0300

    homework

---
+++ src/com/javaguru/student_ivan_shulga/lesson_5/level_4_junior/Task_25.java
@@ -0,0 +1,51 @@
+package com.javaguru.student_ivan_shulga.lesson_5.level_4_junior;
+
+import java.util.Scanner;
+
+class Task_25 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Пожалуйста, введите желаемую длину массива (максимум 10)!");    // для практики ограничили длинну массива 10-ю ячейками
+
+        int arrayLength = scanner.nextInt(); // получаем желаемую пользователем длинну массива
+
+        if (arrayLength <= 10) {   // проверяем условие ограничения, если <= 10, то пользователь заполняет значения, если нет, то завершаем программу
+
+            System.out.println("-----");
+            System.out.println("Создан массив с длинной равной: " + arrayLength);
+            System.out.println("-----");
+
+                int[] newArray = new int[arrayLength];
+
+                    for (int i = 0; i < newArray.length; i++) {
+                        System.out.println("Введите значение ячейки массива под индексом " + i);
+                        newArray[i] = scanner.nextInt();
+                        System.out.println("-----");
+                        System.out.println("Ячейке с индексом " + i + " присвоено значение " + newArray[i]);
+                        System.out.println("-----");
+                    }
+
+                        System.out.println("В итоге вы получили массив со следующими значениями:");
+
+                            for (int i = 0; i < newArray.length; i++) {
+                                System.out.println("Ячейка " + i + " равна " + newArray[i]);
+                            }
+
+        } else {
+            System.out.println("Введена неверная длинна массива! Пожалуйста, перезапустите программу и введите желаемую длину массива (максимум 10)!");
+        }
+
+    }
+
+}
+
+/*
+Напишите программу, в которой:
+- запросите у пользователя с консоли длину массива
+- создайте массив указанной пользователем длины
+- заполните массив числами полученными от пользователя
+- распечатайте на консоль все элементы массива.
+ */
---
+++ src/com/javaguru/student_ivan_shulga/lesson_5/level_4_junior/Task_30.java
@@ -0,0 +1,40 @@
+package com.javaguru.student_ivan_shulga.lesson_5.level_4_junior;
+
+import java.util.Random;
+
+class Task_30 {
+
+    public static void main(String[] args) {
+
+        int[] intArray = new int[5];    // создаём массив
+
+        Random randomInt = new Random();
+
+        for (int i = 0; i < intArray.length; i++) {        // заполняем все ячейки рандомными числами
+            intArray[i] = randomInt.nextInt(100);   // ограничение максимального значения рандомного числа
+        }
+
+        for (int i = 0; i < intArray.length; i++) {     // выводим массив на консоль
+            System.out.println(i + "  -->  " + intArray[i]);
+        }
+
+        System.out.println("Нечётные числа в данном массиве это:");
+
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i] % 2 == 1) {
+                System.out.println(intArray[i]);
+            }
+
+        }
+
+    }
+
+}
+
+/*
+Напишите программу, в которой:
+- создайте массив произвольной длины
+- заполните массив случайными числами
+- распечатайте на консоль все элементы массива
+- найдите все нечётные числа в массиве и выведети их на консоль.
+ */
