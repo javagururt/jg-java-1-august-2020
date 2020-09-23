cbd6bf1331f9620b117cf45929162b637529ece8
Nikolajs <n.sersnovs@gmail.com>
2020-09-24 00:09:30 +0300

    Please check

--- src/com/javaguru/student_nikolajs_sersnovs/lesson_4/level_5_middle/LightColorDetector.java
+++ src/com/javaguru/student_nikolajs_sersnovs/lesson_4/level_5_middle/LightColorDetector.java
@@ -10,22 +10,22 @@ class LightColorDetector {
 
     public String detect(int wavelength) {
 
-        if (wavelength >= 380 & wavelength <= 449) {
+        if (wavelength >= 380 && wavelength <= 449) {
             return "Violet";
         }
-        if (wavelength >= 450 & wavelength <= 494) {
+        if (wavelength >= 450 && wavelength <= 494) {
             return "Blue";
         }
-        if (wavelength >= 495 & wavelength <= 569) {
+        if (wavelength >= 495 && wavelength <= 569) {
             return "Green";
         }
-        if (wavelength >= 570 & wavelength <= 589) {
+        if (wavelength >= 570 && wavelength <= 589) {
             return "Yellow";
         }
-        if (wavelength >= 590 & wavelength <= 619) {
+        if (wavelength >= 590 && wavelength <= 619) {
             return "Orange";
         }
-        if (wavelength >= 620 & wavelength <= 750) {
+        if (wavelength >= 620 && wavelength <= 750) {
             return "Red";
         } else {
             return "Invisible Light";
