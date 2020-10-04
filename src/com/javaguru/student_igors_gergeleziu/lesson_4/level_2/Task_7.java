266d748e7260226746784f220eea6e1ee59492a5
MrJohnyRony <disqueeze@mail.ru>
2020-10-04 17:20:54 +0300

    Done

---
+++ src/com/javaguru/student_igors_gergeleziu/lesson_4/level_2/Task_7.java
@@ -0,0 +1,15 @@
+package com.javaguru.student_igors_gergeleziu.lesson_4.level_2;
+
+import java.util.Scanner;
+
+class Task_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int number1 = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int number2 = sc.nextInt();
+        if (number1 == number2) System.out.println("Numbers " + number1 + " and " + number2 + " are equal");
+        else System.out.println("Numbers " + number1 + " and " + number2 + " are different");
+    }
+}
