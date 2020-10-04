a0c64135d1827ec19e24d316ccd7f5f319f7384d
Maks <maksims.morozovs.11@gmail.com>
2020-10-05 01:02:42 +0300

    Homework

--- src/com/javaguru/teacher/lesson_6/lessoncode/BookUtils.java
+++
@@ -1,9 +0,0 @@
-package com.javaguru.teacher.lesson_6.lessoncode;
-
-class BookUtils {
-
-    static void printInformation(Book book) {
-        System.out.println("Book title = " + book.getTitle());
-        System.out.println("Book pages = " + book.getPages());
-    }
-}
--- src/com/javaguru/teacher/lesson_6/lessoncode/NoiseLevelDetectorTest.java
+++
@@ -1,9 +0,0 @@
-package com.javaguru.teacher.lesson_6.lessoncode;
-
-class NoiseLevelDetectorTest {
-
-    public static void main(String[] args) {
-        String detectResult = NoiseDetector.detect(10);
-        System.out.println(detectResult);
-    }
-}
