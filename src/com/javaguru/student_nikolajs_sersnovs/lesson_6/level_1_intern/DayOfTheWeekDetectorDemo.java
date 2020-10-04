ed8ba59db9844370d68ffe50a644606f1b094bed
MrJohnyRony <disqueeze@mail.ru>
2020-10-04 15:10:49 +0300

    Had to change -> to :, because my main function doesnt work :D

--- src/com/javaguru/student_nikolajs_sersnovs/lesson_6/level_1_intern/DayOfTheWeekDetectorDemo.java
+++ src/com/javaguru/student_nikolajs_sersnovs/lesson_6/level_1_intern/DayOfTheWeekDetectorDemo.java
@@ -11,13 +11,13 @@ class DayOfTheWeekDetectorDemo {
         int firstNumber = myInput.nextInt();
         if ((firstNumber >= 1) && (firstNumber <= 7)) {
             switch (firstNumber) {
-                case 1 -> System.out.println("Monday");
-                case 2 -> System.out.println("Tuesday");
-                case 3 -> System.out.println("Wednesday");
-                case 4 -> System.out.println("Thursday");
-                case 5 -> System.out.println("Friday");
-                case 6 -> System.out.println("Saturday");
-                case 7 -> System.out.println("Sunday");
+                case 1 : System.out.println("Monday");
+                case 2 : System.out.println("Tuesday");
+                case 3 : System.out.println("Wednesday");
+                case 4 : System.out.println("Thursday");
+                case 5 : System.out.println("Friday");
+                case 6 : System.out.println("Saturday");
+                case 7 : System.out.println("Sunday");
             }
         } else {
             System.out.println("Please input a valid number between 1 and 7");
