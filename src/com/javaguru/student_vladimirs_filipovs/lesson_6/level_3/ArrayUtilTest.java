4d768a71952e6e4c3d06b0004d2988ca2caf16cd
VladimirsFilipovs <70902436+VladimirsFilipovs@users.noreply.github.com>
2020-10-05 18:48:16 +0300

    Homework lesson_6, level_3 убрал ошибочное решение

--- src/com/javaguru/student_vladimirs_filipovs/lesson_6/level_3/ArrayUtilTest.java
+++ src/com/javaguru/student_vladimirs_filipovs/lesson_6/level_3/ArrayUtilTest.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 
 class ArrayUtilTest {
 
-    private static final ArrayUtil ARRAY_UTIL = new ArrayUtil();
-
     public static void main(String[] args) {
         ArrayUtilTest testRunner = new ArrayUtilTest();
         testRunner.test1NumberInArray();
@@ -22,75 +20,85 @@ class ArrayUtilTest {
 
 
     void test1NumberInArray() {
-        int[] newArray = ARRAY_UTIL.createArray(5);
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] newArray = arrayUtil.createArray(5);
         int toFindNumber = 3;
         boolean expectedResult = true;
-        boolean actualResult = ARRAY_UTIL.isIncludeArrayAmountOfNumbers(newArray, toFindNumber);
+        boolean actualResult = arrayUtil.isIncludeArrayAmountOfNumbers(newArray, toFindNumber);
         assertResultBoolean(expectedResult, actualResult, "test1NumberInArray");
     }
 
     void test2NumberOutOfArray() {
-        int[] newArray = ARRAY_UTIL.createArray(5);
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] newArray = arrayUtil.createArray(5);
         int toFind = 22;
         boolean expectedResult = false;
-        boolean actualResult = ARRAY_UTIL.isIncludeArrayAmountOfNumbers(newArray, toFind);
+        boolean actualResult = arrayUtil.isIncludeArrayAmountOfNumbers(newArray, toFind);
         assertResultBoolean(expectedResult, actualResult, "test2NumberOutOfArray");
     }
 
     void test3NumberEqualsZero() {
-        int[] newArray = ARRAY_UTIL.createArray(5);
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] newArray = arrayUtil.createArray(5);
         int toFind = 0;
         boolean expectedResult = false;
-        boolean actualResult = ARRAY_UTIL.isIncludeArrayAmountOfNumbers(newArray, toFind);
+        boolean actualResult = arrayUtil.isIncludeArrayAmountOfNumbers(newArray, toFind);
         assertResultBoolean(expectedResult, actualResult, "test3NumberEqualsZero");
     }
 
     void test4HowManyTimesIsRepeatedCurrentNumber() {
+        ArrayUtil arrayUtil = new ArrayUtil();
         int[] newArray = new int[]{12, 4, 5, 99, 66, 31, 12, 99, 99, 81, 75, 99};
         int expectedResult = 4;
-        int actualResult = ARRAY_UTIL.currentNumberCounter(newArray, 99);
+        int actualResult = arrayUtil.currentNumberCounter(newArray, 99);
         assertResult(expectedResult, actualResult, "test4HowManyTimesIsRepeatedCurrentNumber");
     }
 
     void test5HowManyTimesIsRepeatedCurrentNumberNotRepeated() {
+        ArrayUtil arrayUtil = new ArrayUtil();
         int[] newArray = new int[]{12, 4, 5, 99, 66, 31, 12, 99, 99, 81, 75, 99};
         int expectedResult = 0;
-        int actualResult = ARRAY_UTIL.currentNumberCounter(newArray, 7);
+        int actualResult = arrayUtil.currentNumberCounter(newArray, 7);
         assertResult(expectedResult, actualResult, "test5HowManyTimesIsRepeatedCurrentNumberNotRepeated");
     }
 
     void test6ReplaceFirstNumberInArray() {
-        int[] newArray = ARRAY_UTIL.createArray(7);
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] newArray = arrayUtil.createArray(7);
         int[] expectedResult = {777, 2, 3, 4, 5, 6, 7};
-        int[] actualResult = ARRAY_UTIL.replace(newArray, 777);
+        int[] actualResult = arrayUtil.replace(newArray, 777);
         assertResultArray(expectedResult, actualResult, "test6ReplaceFirstNumberInArray");
     }
 
     void test7ReplaceNumberInArray() {
-        int[] newArray = ARRAY_UTIL.createArray(7);
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] newArray = arrayUtil.createArray(7);
         int[] expectedResult = {1, 2, 777, 4, 5, 6, 7};
-        int[] actualResult = ARRAY_UTIL.replaceAll(newArray, 3, 777);
+        int[] actualResult = arrayUtil.replaceAll(newArray, 3, 777);
         assertResultArray(expectedResult, actualResult, "test7ReplaceNumberInArray");
     }
 
     void test8ReplaceNumberInArrayIsNotExist() {
-        int[] newArray = ARRAY_UTIL.createArray(7);
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] newArray = arrayUtil.createArray(7);
         int[] expectedResult = {1, 2, 3, 4, 5, 6, 7};
-        int[] actualResult = ARRAY_UTIL.replaceAll(newArray, 22, 777);
+        int[] actualResult = arrayUtil.replaceAll(newArray, 22, 777);
         assertResultArray(expectedResult, actualResult, "test8ReplaceNumberInArrayIsNotExist");
     }
 
     void test9OppositeArray() {
-        int[] newArray = ARRAY_UTIL.createArray(7);
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] newArray = arrayUtil.createArray(7);
         int[] expectedResult = {7, 6, 5, 4, 3, 2, 1};
-        int[] actualResult = ARRAY_UTIL.oppositeArray(newArray);
+        int[] actualResult = arrayUtil.oppositeArray(newArray);
         assertResultArray(expectedResult, actualResult, "test9OppositeArray");
     }
 
     void test10SortArray() {
+        ArrayUtil arrayUtil = new ArrayUtil();
         int[] newArray = new int[]{21, 54, 32, 69, 777, 69};
         int[] expectedResult = {21, 32, 54, 69, 69, 777};
-        int[] actualResult = ARRAY_UTIL.sortArray(newArray);
+        int[] actualResult = arrayUtil.sortArray(newArray);
         assertResultArray(expectedResult, actualResult, "test10SortArray");
     }
 
