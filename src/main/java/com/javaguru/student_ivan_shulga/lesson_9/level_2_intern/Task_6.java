1a0076e72eb88464e4e5b960c5b0d1665d7ddde9
IvanShulga <glik@bk.ru>
2020-10-14 23:03:31 +0300

    homework

---
+++ src/main/java/com/javaguru/student_ivan_shulga/lesson_9/level_2_intern/Task_6.java
@@ -0,0 +1,25 @@
+package com.javaguru.student_ivan_shulga.lesson_9.level_2_intern;
+
+class Task_6 {
+
+
+
+}
+/*
+Исправьте код программы:
+
+class Mother {
+
+}
+
+class Father {
+
+}
+
+class Child extends Mother, Father {
+
+}
+
+PS: вносить изменения можно только в объявление класса Child.
+Правильных решений у этой задачи два, выбирайте любое.
+ */
