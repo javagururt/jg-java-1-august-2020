a26e080f1baadfc00a3f560ed4a31ba616f9a457
Игорь <revqqq@gmail.com>
2020-10-26 21:17:34 +0200

    Teachers code

--- src/main/java/com/javaguru/student_igors_jurkovs/personal/user_array/UserMenu.java
+++
@@ -1,8 +0,0 @@
-package com.javaguru.student_igors_jurkovs.personal.user_array;
-
-public interface UserMenu {
-
-    void execute();
-
-    String getMenuName();
-}
--- src/main/java/com/javaguru/student_igors_jurkovs/personal/user_array/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaguru.student_igors_jurkovs.personal.user_array;
-
-public interface UserRepository {
-
-    User save(User user);
-
-    User findById(String id);
-
-}
