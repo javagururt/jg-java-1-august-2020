b2ebddedba2223fecc00ef77b18c375a34fc4746
Игорь <revqqq@gmail.com>
2020-11-17 01:32:05 +0200

    Lesson 14 complete

--- src/main/java/com/javaguru/student_igors_jurkovs/lesson_14/level_1_intern/ApplePredicate.java
+++
@@ -1,8 +0,0 @@
-package com.javaguru.student_igors_jurkovs.lesson_14.level_1_intern;
-
-@FunctionalInterface
-public interface ApplePredicate {
-
-    boolean test (Apple apple);
-
-}
--- src/main/java/com/javaguru/student_igors_jurkovs/lesson_14/level_1_intern/FruitStorage.java
+++ src/main/java/com/javaguru/student_igors_jurkovs/lesson_14/level_1_intern/FruitStorage.java
@@ -1,5 +1,8 @@
 package com.javaguru.student_igors_jurkovs.lesson_14.level_1_intern;
 
+import com.javaguru.student_igors_jurkovs.lesson_14.level_1_intern.domain.Apple;
+import com.javaguru.student_igors_jurkovs.lesson_14.level_1_intern.predicate.ApplePredicate;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
