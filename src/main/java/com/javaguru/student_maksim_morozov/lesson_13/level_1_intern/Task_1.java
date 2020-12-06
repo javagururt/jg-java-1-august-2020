9bbfe2c38ac2de950d24947bdbc20acdbbe5c063
Maks <maksims.morozovs.11@gmail.com>
2020-12-07 00:51:56 +0200

    Homework(Check,please)

---
+++ src/main/java/com/javaguru/student_maksim_morozov/lesson_13/level_1_intern/Task_1.java
@@ -0,0 +1,7 @@
+package com.javaguru.student_maksim_morozov.lesson_13.level_1_intern;
+
+class Task_1 {
+    class MyException extends Throwable {
+
+    }
+}
---
+++ src/main/java/com/javaguru/student_maksim_morozov/lesson_13/level_1_intern/Task_4.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_maksim_morozov.lesson_13.level_1_intern;
+
+class Task_4 {
+    class MyException extends Exception {
+
+        public MyException(String message) {super(message);
+        }
+    }
+}
+
