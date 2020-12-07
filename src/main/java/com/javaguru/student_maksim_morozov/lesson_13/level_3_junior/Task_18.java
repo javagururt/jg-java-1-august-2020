01a3a21d540412ff2b73b98c0fdf794e6427607f
Maks <maksims.morozovs.11@gmail.com>
2020-12-07 15:36:22 +0200

    Homework(Check,please)

---
+++ src/main/java/com/javaguru/student_maksim_morozov/lesson_13/level_3_junior/Task_18.java
@@ -0,0 +1,10 @@
+package com.javaguru.student_maksim_morozov.lesson_13.level_3_junior;
+
+class Task_18 {
+    static class NullPointerExceptionDemo{
+        public static void main(String[] args){
+            Integer number = null;
+            Integer sum = number +number;
+        }
+    }
+}
