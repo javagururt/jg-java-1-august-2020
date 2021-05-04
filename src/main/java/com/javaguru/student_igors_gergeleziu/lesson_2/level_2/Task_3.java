ad67f04d0982a2cc671fb92c1fc9a90163a5a98c
shldenis <69810683+shldenis@users.noreply.github.com>
2021-05-04 22:27:41 +0300

    Delete src/main/java/com/javaguru/student_igors_gergeleziu directory

--- src/main/java/com/javaguru/student_igors_gergeleziu/lesson_2/level_2/Task_3.java
+++
@@ -1,9 +0,0 @@
-package com.javaguru.student_igors_gergeleziu.lesson_2.level_2;
-
-import com.javaguru.teacher.codereview.CodeReview;
-
-//Дан код, нужно исправить ошибку компиляции.
-@CodeReview(approved = true)
-class Book {
-
-}
--- src/main/java/com/javaguru/student_igors_gergeleziu/lesson_6/level_5_and_6/TicTacToeGame.java
+++
@@ -1,8 +0,0 @@
-package com.javaguru.student_igors_gergeleziu.lesson_6.level_5_and_6;
-
-class TicTacToeGame {
-    public static void main(String[] args) {
-        TicTacToe game= new TicTacToe();
-        game.play();
-    }
-}
